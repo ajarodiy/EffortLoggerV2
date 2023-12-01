4bcdd09b5b988d2c640165587d0b668aaaec49e2
ishankavdia <ishankavdia93@gmail.com>
2023-11-30 21:59:40 -0700

    Added documentation

--- src/main/java/application/effortloggerv2/EffortLogDisplayPage.java
+++ src/main/java/application/effortloggerv2/EffortLogDisplayPage.java
@@ -1,3 +1,5 @@
+// Author: Ishan Kavdia
+
 package application.effortloggerv2;
 
 import javafx.application.Application;
@@ -8,16 +10,29 @@ import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-// Author: Ishan Kavdia
+/**
+ * The EffortLogDisplayPage class extends the JavaFX Application class and serves as the user interface
+ * for displaying logged efforts in the Effort Logger application.
+ *
+ * @author Ishan Kavdia
+ */
 public class EffortLogDisplayPage extends Application {
     private TableView<EffortLogEntry> tableView;
     private Stage primaryStage;
+
+    /**
+     * The start method is the entry point for the EffortLogDisplayPage application.
+     * It initializes the stage, sets up the UI components, and displays the Effort Log data.
+     *
+     * @param stage The primary stage for this application.
+     */
     @Override
     public void start(Stage stage) {
+        // Set the primary stage
         primaryStage = stage;
         stage.setTitle("Effort Log Display");
 
-        // Create table columns
+        // Create table columns with corresponding data bindings
         TableColumn<EffortLogEntry, String> usernameColumn = new TableColumn<>("Employee Name");
         usernameColumn.setCellValueFactory(cellData -> cellData.getValue().usernameProperty());
 
@@ -50,16 +65,22 @@ public class EffortLogDisplayPage extends Application {
         // Fetch data from the database and populate the TableView
         DatabaseConnector.fetchData(tableView);
 
+        // Create "Home Page" button with an event handler to navigate back to the main page
         Button backButton = new Button("Home Page");
         backButton.setOnAction(event -> Utility.openPage("effortloggingpage", stage));
 
+        // Create a vertical box layout to organize TableView and the "Home Page" button
         VBox vbox = new VBox();
         vbox.getChildren().addAll(tableView, backButton);
 
-        // Create the scene
+        // Create the scene with the specified dimensions
         Scene scene = new Scene(vbox, 1000, 800);
         stage.setScene(scene);
+
+        // Center the stage on the screen
         stage.centerOnScreen();
+
+        // Show the stage
         stage.show();
     }
 
